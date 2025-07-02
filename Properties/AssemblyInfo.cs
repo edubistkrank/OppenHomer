498caccba8117762023e98ae65b4115f5aa88fb0
edubistkrank <edubistkrank@gmail.com>
2025-07-02 17:01:08 +0200

    Add files via upload

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("OppenHomer")]
+[assembly: AssemblyDescription("Opening new home possibilities with natural barriers for Survival: Fountain of Youth")]
+[assembly: AssemblyCompany("3du - Noob Pirate Developer")]
+[assembly: AssemblyProduct("OppenHomer")]
+[assembly: AssemblyCopyright("Copyright © 3du - Noob Pirate Developer 2025")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("730db41e-18c0-4148-bcd8-12e236786a19")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
